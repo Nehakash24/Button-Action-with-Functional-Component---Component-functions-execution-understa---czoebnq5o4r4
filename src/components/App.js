1faf4594af3eeca9b9e16ae6ef3c726b8bd1e9e1
Neha kashyap <32489573+NENNU@users.noreply.github.com>
2023-03-10 13:22:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+function display(){
+  <div id="dis">
+Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+    </div>
+}
 function App() {
   return (
     <div id="main">
-      // Do not alter the main div
+    <button onClick={display}>
+    </button>
     </div>
   );
 }
-
 
 export default App;
 
